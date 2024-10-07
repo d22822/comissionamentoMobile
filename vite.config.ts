b03b1023af75bf1746bc6dc91df307152d32fe07
RAFAEL DE ANDRADE ALVES <rafael.dandrade@tsaengenharia.com>
2024-10-07 15:03:09 -0300

    Correct files final

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: './',
   plugins: [react()], 
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  }
+  
 })
 
